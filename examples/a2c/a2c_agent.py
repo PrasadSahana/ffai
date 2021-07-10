833cfa2183aa7f5becb811be2f0558eb4b5aaaf2
Sahana Prasad <sahana30p@gmail.com>
2021-07-10 18:32:16 +0200

    enabled pathfinding

--- examples/a2c/a2c_agent.py
+++ examples/a2c/a2c_agent.py
@@ -296,7 +296,7 @@ if __name__ == "__main__":
     # Load configurations, rules, arena and teams
     config = ffai.load_config("bot-bowl-iii")
     config.competition_mode = False
-    config.pathfinding_enabled = False
+    config.pathfinding_enabled = True
     ruleset = ffai.load_rule_set(config.ruleset)
     arena = ffai.load_arena(config.arena)
     home = ffai.load_team_by_filename("human", ruleset)
